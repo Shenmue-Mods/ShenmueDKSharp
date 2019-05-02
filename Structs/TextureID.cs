bc18b1eca7ac99092c5011a0e3565427f8713316
PhilYeahz <philyeahz@googlemail.com>
2019-05-02 23:30:54 +0200

    Fixed stack-overflow

--- Structs/TextureID.cs
+++ Structs/TextureID.cs
@@ -42,7 +42,7 @@ namespace ShenmueDKSharp.Structs
 
         public static bool operator ==(TextureID id1, TextureID id2)
         {
-            if (id1 == null || id2 == null) return false;
+            if (id1 is null || id2 is null) return false;
             return id1.Data == id2.Data;
         }
 
