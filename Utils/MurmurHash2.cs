afeb13578cc276dfb5e32249472c99acb1f6ac2b
PhilYeahz <philyeahz@googlemail.com>
2019-06-21 21:00:32 +0200

    Added compact MurmurHash2 function

--- Utils/MurmurHash2.cs
+++ Utils/MurmurHash2.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace ShenmueDKSharp.Utils
 {
+    public class FileHash
+    {
+        public bool HasSecondHash;
+
+        public string FilePath;
+        public string FilePathWithHash;
+
+        public UInt32 FilePathHash;
+        public UInt32 Hash;
+        public UInt32 FinalHash;
+    }
+
     /// <summary>
     /// MurmurHash2 Shenmue implementation
     /// </summary>
@@ -79,6 +91,8 @@ namespace ShenmueDKSharp.Utils
             return hash;
         }
 
+
+
         public static string GetFullFilename(string filename, uint hash, bool includeHash = true)
         {
             string newFilename = filename;
@@ -92,6 +106,69 @@ namespace ShenmueDKSharp.Utils
                 return String.Format("{0}{1}.{2}", Prefix, newFilename, hash.ToString("x8"));
             }
             return String.Format("{0}{1}", Prefix, newFilename);
+        }
+
+        public static FileHash GetFileHash(string filepath, bool secondHash = true)
+        {
+            filepath = filepath.Replace("\\", "/").ToLower();
+            if (filepath[0] != '/')
+            {
+                filepath = "/" + filepath;
+            }
+
+            Console.WriteLine(filepath);
+
+            FileHash hash = new FileHash();
+            hash.FilePath = filepath;
+            hash.HasSecondHash = secondHash;
+
+            if (secondHash)
+            {
+                hash.FilePathHash = Hash(Encoding.ASCII.GetBytes(filepath), (uint)filepath.Length);
+
+                //cutoff the ?usage=0 or other parameters away
+                int cutoffIndex = filepath.LastIndexOf('?');
+                if (cutoffIndex > 0)
+                {
+                    filepath = filepath.Substring(0, cutoffIndex);
+                }
+
+                hash.FilePathWithHash = String.Format("{0}.{1}.00000000", filepath, hash.FilePathHash.ToString("x8"));
+
+                // Remove ending zero
+                filepath = hash.FilePathWithHash.Substring(0, hash.FilePathWithHash.Length - 9);
+
+            }
+
+            // Add asset prefix
+            filepath = Prefix + filepath;
+
+            // Remove dot
+            if (filepath[0] == '.')
+            {
+                filepath = filepath.Substring(1);
+            }
+
+            // Remove slashes and dashes
+            filepath = filepath.Replace("/", "").Replace("-", "");
+
+            // To lower
+            filepath = filepath.ToLower();
+
+            // hash stripped filepath
+            hash.Hash = Hash(Encoding.ASCII.GetBytes(filepath), (uint)filepath.Length);
+
+            // get string length
+            uint totalLength = (uint)filepath.Length;
+            if (secondHash)
+            {
+                totalLength += 9; //include ending zeros
+            }
+
+            // calculate final hash
+            hash.FinalHash = hash.Hash * 0x0001003F + (uint)filepath.Length * totalLength * 0x0002001F;
+
+            return hash;
         }
 
         public static byte[] GetFilenameHash(string filename, bool hasHash = true)
